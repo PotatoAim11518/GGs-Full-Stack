83731ad5ca861800ed82f31e097e679e9957d3de
Anthony <anthony.a.s.martin@gmail.com>
2021-06-01 17:22:13 -0400

    make user sign up form

--- express-project-starter/routes/users.js
+++ express-project-starter/routes/users.js
@@ -1,9 +1,34 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { csrfProtection, asyncHandler } = require('./utils')
+
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('users/signup', csrfProtection, (req, res, next)=> {
+  const user = db.User.build();
+  res.render('user-signup',{ title: "New User Sign Up",
+  user,
+  csrfToken: req.csrfToken(),
+  });
+});
+
+router.post('/users/signup', csrfProtection, asyncHandler(async(req, res, next) => {
+  const {emailAddress,firstName,lastName,hashedPassword} = req.body;
+
+  const user = db.User.build({
+    username,
+    firstName,
+    lastName,
+    email,
+    hashedPassword
+  });
+
+}))
+
 
 module.exports = router;
