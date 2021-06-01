9d9442e2be3fb37c11f8bca1ea24b5f443949ada
Alex <alex.hertel87@gmail.com>
2021-06-01 16:19:30 -0500

    updated formatting

--- express-project-starter/routes/users.js
+++ express-project-starter/routes/users.js
@@ -1,96 +1,58 @@
 const express = require('express');
-const {
-  check,
-  validationResult
-} = require('express-validator');
+const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 
 const db = require('../db/models');
-const {
-  csrfProtection,
-  asyncHandler
-} = require('./utils');
-const {
-  loginUser,
-  logoutUser
-} = require('../auth');
+const { csrfProtection, asyncHandler } = require('./utils');
+const { loginUser, logoutUser } = require('../auth');
 
 const router = express.Router();
 
 const userValidators = [
   check('firstName')
-  .exists({
-    checkFalsy: true
-  })
-  .withMessage('Please provide a value for First Name')
-  .isLength({
-    max: 50
-  })
-  .withMessage('First Name must not be more than 50 characters long'),
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for First Name')
+    .isLength({ max: 50 })
+    .withMessage('First Name must not be more than 50 characters long'),
   check('lastName')
-  .exists({
-    checkFalsy: true
-  })
-  .withMessage('Please provide a value for Last Name')
-  .isLength({
-    max: 50
-  })
-  .withMessage('Last Name must not be more than 50 characters long'),
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Last Name')
+    .isLength({ max: 50 })
+    .withMessage('Last Name must not be more than 50 characters long'),
   check('emailAddress')
-  .exists({
-    checkFalsy: true
-  })
-  .withMessage('Please provide a value for Email Address')
-  .isLength({
-    max: 255
-  })
-  .withMessage('Email Address must not be more than 255 characters long')
-  .isEmail()
-  .withMessage('Email Address is not a valid email')
-  .custom((value) => {
-    return db.User.findOne({
-        where: {
-          emailAddress: value
-        }
-      })
-      .then((user) => {
-        if (user) {
-          return Promise.reject('The provided Email Address is already in use by another account');
-        }
-      });
-  }),
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Email Address')
+    .isLength({ max: 255 })
+    .withMessage('Email Address must not be more than 255 characters long')
+    .isEmail()
+    .withMessage('Email Address is not a valid email')
+    .custom((value) => {
+      return db.User.findOne({ where: { emailAddress: value } })
+        .then((user) => {
+          if (user) {
+            return Promise.reject('The provided Email Address is already in use by another account');
+          }
+        });
+    }),
   check('password')
-  .exists({
-    checkFalsy: true
-  })
-  .withMessage('Please provide a value for Password')
-  .isLength({
-    max: 50
-  })
-  .withMessage('Password must not be more than 50 characters long')
-  .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, 'g')
-  .withMessage('Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")'),
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Password')
+    .isLength({ max: 50 })
+    .withMessage('Password must not be more than 50 characters long')
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, 'g')
+    .withMessage('Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")'),
   check('confirmPassword')
-  .exists({
-    checkFalsy: true
-  })
-  .withMessage('Please provide a value for Confirm Password')
-  .isLength({
-    max: 50
-  })
-  .withMessage('Confirm Password must not be more than 50 characters long')
-  .custom((value, {
-    req
-  }) => {
-    if (value !== req.body.password) {
-      throw new Error('Confirm Password does not match Password');
-    }
-    return true;
-  }),
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Confirm Password')
+    .isLength({ max: 50 })
+    .withMessage('Confirm Password must not be more than 50 characters long')
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Confirm Password does not match Password');
+      }
+      return true;
+    }),
 ];
-
-
-
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
