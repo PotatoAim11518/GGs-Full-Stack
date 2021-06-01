bec26b1ee27633711721382605ad651ac1fb94be
wilson <whuang3015@gmail.com>
2021-06-01 14:45:30 -0700

    Add user login GET route and begin working on POST route

--- express-project-starter/routes/users.js
+++ express-project-starter/routes/users.js
@@ -1,9 +1,51 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
+const { User } = require('../db/models');
+const router = express.Router();
+const { csrfProtection , asyncHandler } = require('./utils');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+// GET /users/login
+router.get('/login', csrfProtection, asyncHandler( (res, req) => {
+  res.render('login', { title: 'Login', csrfToken = req.csrfToken() });
+}));
+
+// POST /users/login
+router.post('/login', csrfProtection, asyncHandler( (res, req) => {
+  const { username, password } = req.body; // pulls the input data out of the request
+  const user = await User.findOne({ where: { username } }); // tries to find the user in the database
+
+  // let errors = [];
+  const validatorErrors = validationResult(req);
+
+  // check for validation errors...
+  if (!validatorErrors.isEmpty()) {
+
+    if (user) { // if the user was found...
+
+      const passwordMatch = bcrypt.compare(password, user.hashedPassword); // compare the password provided with the stored hashedPassword in the db tied to the user
+
+      if (passwordMatch) {
+        // log the user in
+        return res.redirect('/')
+      }
+    }
+
+  } else {
+    // render the errors
+  }
+
+
+  res.render('login', { title: 'Login',
+
+  })
+}));
+
 
 module.exports = router;
