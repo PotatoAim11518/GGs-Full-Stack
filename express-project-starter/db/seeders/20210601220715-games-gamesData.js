15284df5da3ea2814d8d138153cce12641eee903
Alex <alex.hertel87@gmail.com>
2021-06-01 18:35:26 -0500

    gamesData

--- express-project-starter/db/seeders/20210601220715-games-gamesData.js
+++ express-project-starter/db/seeders/20210601220715-games-gamesData.js
@@ -3,16 +3,135 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
 
-      Example:
-      return queryInterface.bulkInsert('Games', [{
-        name: 'Astroneer',
-        description: "Explore and reshape distant worlds! Astroneer is set during the 25th century Intergalactic Age of Discovery, where Astroneers explore the frontiers of outer space, risking their lives in harsh environments to unearth rare discoveries and unlock the mysteries of the universe.",
-        genreId: ,
-        publisherId: ,
-        platforms: ,
-        imageSrc: ,
-        releaseDate:
-      }], {});
+    Example: return queryInterface.bulkInsert('Games', [{
+      name: 'Astroneer',
+      description: "Explore and reshape distant worlds! Astroneer is set during the 25th century Intergalactic Age of Discovery, where Astroneers explore the frontiers of outer space, risking their lives in harsh environments to unearth rare discoveries and unlock the mysteries of the universe.",
+      genreId: ,
+      publisherId: ,
+      platforms: ,
+      imageSrc: ,
+      releaseDate:
+    }, {
+      name: '',
+      description: ,
+      genreId: ,
+      publisherId: ,
+      platforms: ,
+      imageSrc: ,
+      releaseDate:
+    }, {
+      name: '',
+      description: ,
+      genreId: ,
+      publisherId: ,
+      platforms: ,
+      imageSrc: ,
+      releaseDate:
+    }, {
+      name: '',
+      description: ,
+      genreId: ,
+      publisherId: ,
+      platforms: ,
+      imageSrc: ,
+      releaseDate:
+    }, {
+      name: '',
+      description: ,
+      genreId: ,
+      publisherId: ,
+      platforms: ,
+      imageSrc: ,
+      releaseDate:
+    }, {
+      name: '',
+      description: ,
+      genreId: ,
+      publisherId: ,
+      platforms: ,
+      imageSrc: ,
+      releaseDate:
+    }, {
+      name: '',
+      description: ,
+      genreId: ,
+      publisherId: ,
+      platforms: ,
+      imageSrc: ,
+      releaseDate:
+    }, {
+      name: '',
+      description: ,
+      genreId: ,
+      publisherId: ,
+      platforms: ,
+      imageSrc: ,
+      releaseDate:
+    }, {
+      name: '',
+      description: ,
+      genreId: ,
+      publisherId: ,
+      platforms: ,
+      imageSrc: ,
+      releaseDate:
+    }, {
+      name: '',
+      description: ,
+      genreId: ,
+      publisherId: ,
+      platforms: ,
+      imageSrc: ,
+      releaseDate:
+    }, {
+      name: '',
+      description: ,
+      genreId: ,
+      publisherId: ,
+      platforms: ,
+      imageSrc: ,
+      releaseDate:
+    }, {
+      name: '',
+      description: ,
+      genreId: ,
+      publisherId: ,
+      platforms: ,
+      imageSrc: ,
+      releaseDate:
+    }, {
+      name: '',
+      description: ,
+      genreId: ,
+      publisherId: ,
+      platforms: ,
+      imageSrc: ,
+      releaseDate:
+    }, {
+      name: '',
+      description: ,
+      genreId: ,
+      publisherId: ,
+      platforms: ,
+      imageSrc: ,
+      releaseDate:
+    }, {
+      name: '',
+      description: ,
+      genreId: ,
+      publisherId: ,
+      platforms: ,
+      imageSrc: ,
+      releaseDate:
+    }, {
+      name: '',
+      description: ,
+      genreId: ,
+      publisherId: ,
+      platforms: ,
+      imageSrc: ,
+      releaseDate:
+    }, ], {});
 
   },
 
