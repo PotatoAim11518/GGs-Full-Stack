b977a411db33019699d401eb1c0e9d68198a004a
AnthonyCost <anthonycost99@gmail.com>
2021-06-02 10:05:51 -0700

    added games model, migration and unseeded seed file

---
+++ express-project-starter/db/migrations/20210602170106-create-game.js
@@ -0,0 +1,55 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Games', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING(50),
+        unique:true,
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      genreId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {model: "Genres"}
+      },
+      publisherId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {model: "Publishers"}
+      },
+      platforms: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      imagSrc: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      releaseDate: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Games');
+  }
+};
