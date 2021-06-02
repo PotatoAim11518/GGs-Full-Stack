5af8754452965fb4f9ac6e608f062d2c7f44ee08
Alex <alex.hertel87@gmail.com>
2021-06-02 16:04:49 -0500

    Changed "Reviews" to "Review" in model & migration

--- express-project-starter/db/models/reviews.js
+++ express-project-starter/db/models/reviews.js
@@ -1,16 +1,20 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Reviews = sequelize.define('Reviews', {
+  const Reviews = sequelize.define('Review', {
     userId: DataTypes.INTEGER,
     gameId: DataTypes.INTEGER,
     content: DataTypes.TEXT,
     rating: DataTypes.NUMERIC
   }, {});
-  Reviews.associate = function(models) {
+  Reviews.associate = function (models) {
     // associations can be defined here
     //review belongs to a user
-    Reviews.belongsTo(models.User, {foreignKey: 'userId'});
-    Reviews.belongsTo(models.Game, {foreignKey: 'gameId'});
+    Reviews.belongsTo(models.User, {
+      foreignKey: 'userId'
+    });
+    Reviews.belongsTo(models.Game, {
+      foreignKey: 'gameId'
+    });
   };
   return Reviews;
 };
