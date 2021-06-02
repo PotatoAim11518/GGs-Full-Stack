dfd4e9c59dafe2ec08bdb08b034adda7c5aa1da7
Alex <alex.hertel87@gmail.com>
2021-06-02 16:06:18 -0500

    Changed "Reviews" to "Review" in model/mig

--- express-project-starter/db/migrations/20210602180322-create-reviews.js
+++ express-project-starter/db/migrations/20210602180322-create-reviews.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Reviews', {
+    return queryInterface.createTable('Review', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -37,6 +37,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Reviews');
+    return queryInterface.dropTable('Review');
   }
 };
