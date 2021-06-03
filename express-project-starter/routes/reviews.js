7aa412f62a8ac02776b12fc8f8cb75236c17d943
wilson <whuang3015@gmail.com>
2021-06-03 09:50:09 -0700

    Set up routes for reviews

---
+++ express-project-starter/routes/reviews.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const { Review, Game } = require('../db/models');
+const { requireAuth } = require('../auth');
+const { csrfProtection, asyncHandler} = require('./utils');
+const { check, validationResult } = require('express-validator');
+
+const router = express.Router();
+
+// GET /games/:gameId/reviews
+router.get('/games/:gameId/reviews', asyncHandler((req, res) => {
+  const gameId = parseInt(req.params.gameId, 10);
+  const reviews = await Review.findAll({
+    include: [ Game ],
+    where: {
+      gameId
+    }
+  });
+  // TO DO: figure out how to use the review objects in a template
+  res.render('game', { reviews })
+}));
+
+
+
+// POST /games/:gameId/reviews
+router.post('/games/:gameId/reviews', csrfProtection, requireAuth, asyncHandler((req, res) => {
+  if (req.session.auth) {
+    const gameId = parseInt(req.params.gameId);
+    const { userId } = req.session.auth;
+    const { content, rating } = req.body;
+
+    const review = await Review.create({
+      userId,
+      gameId,
+      content,
+      rating
+    })
+
+  } else {
+    res.redirect('/users/login');
+  }
+}));
+
+// DELETE /:reviewsId
+router.delete('/:reviewsId(\\d+)', asyncHandler((req, res) => {
+
+}));
+module.exports = router;
