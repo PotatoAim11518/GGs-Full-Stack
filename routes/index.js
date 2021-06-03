523b3dbe0b1d86e7f43d72b96f6456c0f9b2d1cc
Anthony <anthony.a.s.martin@gmail.com>
2021-06-03 17:28:45 -0400

    index.js complete and index.pug complete

--- routes/index.js
+++ routes/index.js
@@ -9,14 +9,29 @@ router.get('/', asyncHandler(async (req, res, next) => {
   const randomNumber2 = Math.floor(Math.random()*(17-9)+ 9);
   const randomNumber3 = Math.floor(Math.random()*(25-17)+ 17);
 
-  const game1 = await Game.findByPk(randomNumber1)
-  const game2 = await Game.findByPk(randomNumber2)
-  const game3 = await Game.findByPk(randomNumber3)
-
-  const genres = await Genre.findAll({
-    where:{ id },
-    include: genreName
+  const game1 = await Game.findOne({
+    where:{
+      id: randomNumber1
+    },
+    include: imageSrc
   })
+  
+  const game2 = await Game.findOne({
+    where:{
+      id: randomNumber2
+    },
+    include: imageSrc
+  })
+  
+  const game3 = await Game.findOne({
+    where:{
+      id: randomNumber3
+    },
+    include: imageSrc
+  })
+  
+
+  const genres = await Genre.findAll()
   res.render('index', genres, game1, game2, game3, { title: 'Good Games' });
 }));
 
