991e3e5154a95ee3ce2a6c9f6ab42658e0af4b03
Alex <alex.hertel87@gmail.com>
2021-06-03 16:45:05 -0500

    GOT THE FUCKING THING TO WORK

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,45 @@
 const express = require('express');
 const router = express.Router();
-const { asyncHandler } = require('./utils')
-const { Genre, Game } = require('../db/models');
+const {
+  asyncHandler
+} = require('./utils')
+const {
+  Genre,
+  Game
+} = require('../db/models');
 
 /* GET home page. */
 router.get('/', asyncHandler(async (req, res, next) => {
   const randomNumber1 = Math.floor(Math.random() * 8);
-  const randomNumber2 = Math.floor(Math.random()*(17-9)+ 9);
-  const randomNumber3 = Math.floor(Math.random()*(25-17)+ 17);
+  const randomNumber2 = Math.floor(Math.random() * (17 - 9) + 9);
+  const randomNumber3 = Math.floor(Math.random() * (25 - 17) + 17);
 
   const game1 = await Game.findOne({
-    where:{
+    where: {
       id: randomNumber1
     },
-    include: imageSrc
   })
-  
+
   const game2 = await Game.findOne({
-    where:{
+    where: {
       id: randomNumber2
     },
-    include: imageSrc
   })
-  
+
   const game3 = await Game.findOne({
-    where:{
+    where: {
       id: randomNumber3
     },
-    include: imageSrc
   })
-  
 
-  const genres = await Genre.findAll()
-  res.render('index', genres, game1, game2, game3, { title: 'Good Games' });
+  const genres = await Genre.findAll();
+  res.render('index', {
+    title: 'Good Games',
+    genres,
+    game1,
+    game2,
+    game3,
+  });
 }));
 
 module.exports = router;
