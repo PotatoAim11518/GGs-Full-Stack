2a041f70d57a8449353e1627bb4db9f41767c4a3
wilson <whuang3015@gmail.com>
2021-06-03 16:06:31 -0700

    Complete preliminary reviews routes

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const { Review, Game } = require('../db/models');
-const { requireAuth } = require('../auth');
+const { authUser } = require('../auth');
 const { csrfProtection, asyncHandler} = require('./utils');
 const { check, validationResult } = require('express-validator');
 
 const router = express.Router();
 
 // GET /games/:gameId/reviews
-router.get('/games/:gameId/reviews', asyncHandler(async (req, res) => {
+router.get('/games/:gameId/reviews', asyncHandler((req, res) => {
   const gameId = parseInt(req.params.gameId, 10);
   const reviews = await Review.findAll({
     include: [ Game ],
@@ -16,40 +16,68 @@ router.get('/games/:gameId/reviews', asyncHandler(async (req, res) => {
     }
   });
   // TO DO: figure out how to use the review objects in a template
-  res.render('game', { reviews })
+  res.render('reviews', { reviews })
 }));
 
 const reviewValidators = [
   check('rating')
     .exists({ checkFalsy: true })
-    .withMessage('Please rate this game!')
+    .withMessage('Please rate this game!'),
+  check('content')
+    .exists({ checkFalsy: true })
+    .withMessage('Please write a review before submitting!')
 ]
 
 // POST /games/:gameId/reviews
-router.post('/games/:gameId/reviews', csrfProtection, requireAuth, reviewValidators,asyncHandler(async (req, res) => {
-  if (req.session.auth) {
+router.post('/games/:gameId/reviews', authUser, csrfProtection, requireAuth, reviewValidators,asyncHandler(async (req, res) => {
 
-    const validatorErrors = validationResult(req);
+  const { userId } = req.session.auth;
+  const gameId = parseInt(req.params.gameId);
+  const { content, rating } = req.body;
 
-
-    const gameId = parseInt(req.params.gameId);
-    const { userId } = req.session.auth;
-    const { content, rating } = req.body;
-
-    const review = await Review.create({
-      userId,
+  const currentUserReview = await Review.findAll({
+    where: {
       gameId,
-      content,
-      rating
-    })
+      userId,
+    }
+  })
 
+  let errors = [];
+  const validatorErrors = validationResult(req);
+
+  if (validatorErrors.isEmpty()) {
+
+    if (!currentUserReview) {
+      await Review.create({
+        userId,
+        gameId,
+        content,
+        rating
+      });
+    }
+    errors.push('Sorry, you\'ve already written a review for this game.');
   } else {
-    res.redirect('/users/login');
+    errors = validatorErrors.array().map( (error) => error.msg );
   }
+  res.render('reviews', {
+    csrfToken: req.csrfToken(),
+    errors,
+  })
 }));
 
 // DELETE /:reviewsId
-router.delete('/:reviewsId(\\d+)', asyncHandler((req, res) => {
+router.delete('/reviews/:id(\\d+)', authUser, csrfProtection, asyncHandler((req, res) => {
 
+  const { userId } = req.session.auth;
+  const reviewId = req.params.id;
+  const currentUserReview = await Review.findOne({
+    where: {
+      reviewId,
+      userId,
+    }
+  });
+  const { gameId } = currentUserReview
+  await currentUserReview.destroy();
+  res.redirect(`/games/${gameId}`);
 }));
 module.exports = router;
