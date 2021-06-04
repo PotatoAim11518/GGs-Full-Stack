13c161f69054daa2db5b9d057fce5e925e7158da
Alex <alex.hertel87@gmail.com>
2021-06-04 12:01:50 -0500

    renamed file

---
+++ routes/allGenres.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const {
+    Game,
+    Genre,
+    Publisher
+} = require('../db/models');
+const {
+    asyncHandler
+} = require('./utils')
+
+
+const router = express.Router();
+
+
+router.get('games/genreId/:id', asyncHandler(async (req, res) => {
+    const genreId = parseInt(req.params.id, 10)
+    const genre = await Genre.findAll();
+
+    res.render('all-genres', {
+        genres
+    })
+}));
+
+module.exports = router;
