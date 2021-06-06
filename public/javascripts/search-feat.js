09b72abc055cc72d53a3ba465506792587169f2c
Anthony <anthony.a.s.martin@gmail.com>
2021-06-05 22:07:20 -0400

    got search function to work, needs extra styling

--- public/javascripts/search-feat.js
+++ public/javascripts/search-feat.js
@@ -11,18 +11,13 @@ inputBox.onkeyup = (e)=>{
     let userData = e.target.value; //user entered data
     let newArray = [];
     if(userData){
-        icon.onclick = ()=>{
-            webLink = "localhost:8080/games/"+ gameId(gameStuff.id);
-            linkTag.setAttribute("href", webLink);
-            linkTag.click();
-        }
         newArray = suggestions.filter((data)=>{
             //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
             return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase()); 
         });
         newArray = newArray.map((data)=>{
             // passing return data inside li tag
-            return data = '<li>'+`<a href="localhost:8080/games/${titleId(data)}">`+ data +'</a>'+'</li>';
+            return data = '<li>'+`<a href="/games/${titleId(data)}/reviews">`+ data +'</a>'+'</li>';
         });
         searcher.classList.add("active"); //show autocomplete box
         showSuggestions(newArray);
@@ -30,8 +25,6 @@ inputBox.onkeyup = (e)=>{
         for (let i = 0; i < allList.length; i++) {
             //adding onclick attribute in all li tag
             allList[i].setAttribute("onclick", "select(this)");
-            //add achor tag to the attribute that
-            let 
         }
     }else{
         searcher.classList.remove("active"); //hide autocomplete box
@@ -41,11 +34,6 @@ inputBox.onkeyup = (e)=>{
 function select(element){
     let selectData = element.textContent;
     inputBox.value = selectData;
-    icon.onclick = ()=>{
-        webLink = "" + selectData;
-        linkTag.setAttribute("href", webLink);
-        linkTag.click();
-    }
     searcher.classList.remove("active");
 }
 function showSuggestions(list){
