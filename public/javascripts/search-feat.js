222ba9f6965f77d4139de1a5028fe5949ef33eac
Anthony <anthony.a.s.martin@gmail.com>
2021-08-23 22:13:21 -0400

    fix search bar display from scroll issues

--- public/javascripts/search-feat.js
+++ public/javascripts/search-feat.js
@@ -2,12 +2,10 @@ const searcher = document.querySelector(".search-input");
 const inputBox = searcher.querySelector("input");
 const suggestionBox = searcher.querySelector(".autocom-box");
 const icon = searcher.querySelector(".search-icon");
-let linkTag = searcher.querySelector("a");
-
-let webLink;
 
 // if user press any key and release
 inputBox.onkeyup = (e)=>{
+
     let userData = e.target.value; //user entered data
     let newArray = [];
     if(userData){
@@ -15,18 +13,19 @@ inputBox.onkeyup = (e)=>{
             //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
             return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
         });
-        newArray = newArray.map((data)=>{
+        let suggArray = newArray.map((data)=>{
             // passing return data inside li tag
             return data = '<li>'+`<a href="/games/${titleId(data)}/reviews">`+ data +'</a>'+'</li>';
         });
         searcher.classList.add("active"); //show autocomplete box
-        showSuggestions(newArray);
+        showSuggestions(suggArray);
         let allList = suggestionBox.querySelectorAll("li");
         for (let i = 0; i < allList.length; i++) {
             //adding onclick attribute in all li tag
             allList[i].setAttribute("onclick", "select(this)");
         }
     }else{
+        showSuggestions(newArray)
         searcher.classList.remove("active"); //hide autocomplete box
     }
 }
@@ -39,13 +38,19 @@ function select(element){
 function showSuggestions(list){
     let listData;
     if(!list.length){
-        userValue = inputBox.value;
-        listData = '<li>'+ userValue +'</li>';
+        if(inputBox.value !== ''){
+            userValue = inputBox.value;
+            listData = '<li>'+ userValue +'</li>';
+        }else{
+            listData=null
+        }
     }else{
         listData = list.join('');
     }
     suggestionBox.innerHTML = listData;
 }
+
+        
 function titleId(title){
     return suggestions.indexOf(title) + 1;
 }
